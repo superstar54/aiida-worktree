4f777fd773aadc90ea0b56c02d21dc51a6993643
superstar54 <xingwang1991@gmail.com>
2024-01-14 11:39:43 +0100

    add node route

---
+++ aiida_worktree/web/backend/app/datanode.py
@@ -0,0 +1,81 @@
+from fastapi import APIRouter, HTTPException, Query
+from aiida import orm
+
+router = APIRouter()
+
+
+@router.get("/api/datanode-data")
+async def read_datanode_data(search: str = Query(None)):
+    from aiida.orm import QueryBuilder, Data
+
+    try:
+        builder = QueryBuilder()
+        builder.append(
+            Data,
+            filters={"node_type": {"like": f"%{search}%"}} if search else None,
+            tag="data",
+        )
+
+        records = builder.all()
+        data = []
+        for r in records:
+            data.append(
+                {
+                    "pk": r[0].pk,
+                    "uuid": r[0].uuid,
+                    "ctime": r[0].ctime,
+                    "node_type": r[0].node_type,
+                }
+            )
+        return data
+    except KeyError:
+        raise HTTPException(status_code=404, detail=f"Data node {id} not found")
+
+
+@router.get("/api/datanode/{id}")
+async def read_data_node_item(id: int):
+
+    try:
+        node = orm.load_node(id)
+        content = {}
+        if isinstance(node, orm.Int):
+            content["type"] = "Int"
+            content["value"] = node.value
+        elif isinstance(node, orm.Float):
+            content["type"] = "Float"
+            content["value"] = node.value
+        elif isinstance(node, orm.Str):
+            content["type"] = "Str"
+            content["value"] = node.value
+        elif isinstance(node, orm.Bool):
+            content["type"] = "Bool"
+            content["value"] = node.value
+        elif isinstance(node, orm.StructureData):
+            content["type"] = "StructureData"
+            atoms = node.get_ase()
+            content["cell"] = atoms.cell.tolist()
+            content["pbc"] = atoms.pbc.tolist()
+            content["positions"] = atoms.positions.tolist()
+            content["species"] = atoms.get_chemical_symbols()
+        return content
+    except KeyError:
+        raise HTTPException(status_code=404, detail=f"Data node {id} not found")
+
+
+# Route for deleting a datanode item
+@router.delete("/api/datanode/delete/{id}")
+async def delete_data_node(
+    id: int,
+):
+    from aiida.tools import delete_nodes
+
+    try:
+        # Perform the delete action here
+        _, was_deleted = delete_nodes([id], dry_run=False)
+
+        if was_deleted:
+            return {"message": f"Deleted datanode {id}"}
+        else:
+            return {"message": f"Failed to delete datanode {id}"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
